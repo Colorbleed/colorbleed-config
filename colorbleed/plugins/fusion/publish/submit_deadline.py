1a5cf732f5b8708acd418e2c00276a05215b82fa
wijnand <wikoreman@gmail.com>
2018-04-06 10:01:15 +0200

    Added AVALON_TOOLS

--- colorbleed/plugins/fusion/publish/submit_deadline.py
+++ colorbleed/plugins/fusion/publish/submit_deadline.py
@@ -114,22 +114,14 @@ class FusionSubmitDeadline(pyblish.api.InstancePlugin):
             payload["JobInfo"]["OutputFilename%d" % index] = filename
 
         # Include critical variables with submission
-        keys = [
-            # TODO: This won't work if the slaves don't have accesss to
-            # these paths, such as if slaves are running Linux and the
-            # submitter is on Windows.
-            "PYTHONPATH",
-            "OFX_PLUGIN_PATH",
-            "FUSION9_MasterPrefs"
-        ]
-        environment = dict({key: os.environ[key] for key in keys
-                            if key in os.environ}, **api.Session)
+        env = api.Session.copy()
+        env["AVALON_TOOLS"] = "global;fusion{}".format(fusion_version)
 
         payload["JobInfo"].update({
             "EnvironmentKeyValue%d" % index: "{key}={value}".format(
                 key=key,
-                value=environment[key]
-            ) for index, key in enumerate(environment)
+                value=env[key]
+            ) for index, key in enumerate(env)
         })
 
         self.log.info("Submitting..")
