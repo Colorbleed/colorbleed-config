fda2aaa8c3ec626a96025f4fa5b092ba852be62a
wijnand <wikoreman@gmail.com>
2018-04-10 10:39:42 +0200

    renamved env_prototype to acre

--- colorbleed/launcher_actions.py
+++ colorbleed/launcher_actions.py
@@ -1,13 +1,12 @@
 import os
 import pprint
 
-import avalon.pipeline as pipeline
-import avalon.api as avalon
+from avalon import api, pipeline, lib
 
-from env_prototype import api
+import acre
 
 
-class FusionRenderNode(avalon.Action):
+class FusionRenderNode(api.Action):
 
     name = "fusionrendernode"
     label = "F9 Render Node"
@@ -32,22 +31,22 @@ class FusionRenderNode(avalon.Action):
         """
 
         # Update environment with session
-        tools_env = api.get_tools(["global", "fusionnode9"])
+        tools_env = acre.get_tools(["global", "fusionnode9"])
 
-        env = api.compute(tools_env)
-        env = api.merge(env, current_env=dict(os.environ))
+        env = acre.compute(tools_env)
+        env = acre.merge(env, current_env=dict(os.environ))
         print("Environment %s" % pprint.pformat(env))
 
-        exe = api.which(self.name, env)
+        exe = acre.which(self.name, env)
         if not exe:
             raise ValueError("Unable to find executable: %s" % self.name)
 
         print("Launching: %s" % exe)
-        return api.execute(exe, environment=env, args=[])
+        return lib.launch(exe, environment=env, args=[])
 
 
 def register_launcher_actions():
     """Register specific actions which should be accessible in the launcher"""
 
     # Register fusion actions
-    pipeline.register_plugin(avalon.Action, FusionRenderNode)
+    pipeline.register_plugin(api.Action, FusionRenderNode)
