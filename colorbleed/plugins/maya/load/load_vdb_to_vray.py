3f2db96c7133e557b0923d14144a91406640de3c
Roy Nieterau <roy_nieterau@hotmail.com>
2019-03-07 18:27:22 +0100

    Implement full loader for VDBCache in Maya for Redshift + V-Ray

--- colorbleed/plugins/maya/load/load_vdb_to_vray.py
+++ colorbleed/plugins/maya/load/load_vdb_to_vray.py
@@ -1,4 +1,5 @@
-from avalon import api
+import os
+from avalon import api, io
 
 
 class LoadVDBtoVRay(api.Loader):
@@ -17,6 +18,20 @@ class LoadVDBtoVRay(api.Loader):
         import avalon.maya.lib as lib
         from avalon.maya.pipeline import containerise
 
+        assert os.path.exists(self.fname), (
+                "Path does not exist: %s" % self.fname
+        )
+
+        # Ensure V-ray is loaded
+        if not cmds.pluginInfo("vrayformaya", query=True, loaded=True):
+            cmds.loadPlugin("vrayformaya")
+
+        # When V-Ray version 4+ (V-Ray Next) then ensure the separate
+        # "vrayvolumegrid" plug-in gets loaded too.
+        if cmds.pluginInfo("vrayformaya", query=True, version=True) == "Next":
+            if not cmds.pluginInfo("vrayvolumegrid", query=True, loaded=True):
+                cmds.loadPlugin("vrayvolumegrid")
+
         # Check if viewport drawing engine is Open GL Core (compat)
         render_engine = None
         compatible = "OpenGLCoreProfileCompat"
@@ -29,7 +44,6 @@ class LoadVDBtoVRay(api.Loader):
                                "set the render engine to '%s'" % compatible)
 
         asset = context['asset']
-        version = context["version"]
 
         asset_name = asset["name"]
         namespace = namespace or lib.unique_namespace(
@@ -42,15 +56,14 @@ class LoadVDBtoVRay(api.Loader):
         label = "{}:{}".format(namespace, name)
         root = cmds.group(name=label, empty=True)
 
-        # Create VR
+        # Create VRayVolumeGrid
         grid_node = cmds.createNode("VRayVolumeGrid",
                                     name="{}VVGShape".format(label),
                                     parent=root)
 
-        # Set attributes
-        cmds.setAttr("{}.inFile".format(grid_node), self.fname, type="string")
-        cmds.setAttr("{}.inReadOffset".format(grid_node),
-                     version["startFrames"])
+        self._apply_settings(grid_node,
+                             path=self.fname,
+                             show_preset_popup=True)
 
         nodes = [root, grid_node]
         self[:] = nodes
@@ -61,3 +74,73 @@ class LoadVDBtoVRay(api.Loader):
             nodes=nodes,
             context=context,
             loader=self.__class__.__name__)
+
+    def _apply_settings(self,
+                        grid_node,
+                        path,
+                        show_preset_popup=True):
+        """Apply the settings for the VDB path to the VRayVolumeGrid"""
+
+        from colorbleed.maya.lib import attribute_values
+        from maya import cmds
+
+        # The path is either a single file or sequence in a folder.
+        if os.path.isfile(path):
+            filename = path
+
+        else:
+            # The path points to the publish .vdb sequence folder so we
+            # find the first file in there that ends with .vdb
+            files = sorted(os.listdir(path))
+            first = next((x for x in files if x.endswith(".vdb")), None)
+            if first is None:
+                raise RuntimeError("Couldn't find first .vdb file of "
+                                   "sequence in: %s" % path)
+            filename = os.path.join(path, first)
+
+        # Suppress preset pop-up if we want.
+        popup_attr = "{0}.inDontOfferPresets".format(grid_node)
+        popup = {popup_attr: not show_preset_popup}
+
+        with attribute_values(popup):
+            cmds.setAttr(grid_node + ".inPath", filename, type="string")
+
+    def update(self, container, representation):
+
+        import maya.cmds as cmds
+
+        path = api.get_representation_path(representation)
+
+        # Find VRayVolumeGrid
+        members = cmds.sets(container['objectName'], query=True)
+        grid_nodes = cmds.ls(members, type="VRayVolumeGrid", long=True)
+        assert len(grid_nodes) == 1, "This is a bug"
+
+        # Update the VRayVolumeGrid
+        self._apply_settings(grid_nodes[0],
+                             path=path,
+                             show_preset_popup=False)
+
+        # Update container representation
+        cmds.setAttr(container["objectName"] + ".representation",
+                     str(representation["_id"]),
+                     type="string")
+
+    def switch(self, container, representation):
+        self.update(container, representation)
+
+    def remove(self, container):
+        import maya.cmds as cmds
+
+        # Get all members of the avalon container, ensure they are unlocked
+        # and delete everything
+        members = cmds.sets(container['objectName'], query=True)
+        cmds.lockNode(members, lock=False)
+        cmds.delete([container['objectName']] + members)
+
+        # Clean up the namespace
+        try:
+            cmds.namespace(removeNamespace=container['namespace'],
+                           deleteNamespaceContent=True)
+        except RuntimeError:
+            pass
