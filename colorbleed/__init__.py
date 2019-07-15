4bb43d0025f29e295ad1a1ef0831f38fb60295d2
Roy Nieterau <roy_nieterau@hotmail.com>
2019-07-15 18:33:11 +0200

    Register "local" target by default

--- colorbleed/__init__.py
+++ colorbleed/__init__.py
@@ -19,8 +19,13 @@ def install():
     pyblish.register_plugin_path(PUBLISH_PATH)
     avalon.register_plugin_path(avalon.Loader, LOAD_PATH)
 
+    # Register default "local" target
+    pyblish.register_target("local")
+
 
 def uninstall():
     print("Deregistering global plug-ins..")
     pyblish.deregister_plugin_path(PUBLISH_PATH)
     avalon.deregister_plugin_path(avalon.Loader, LOAD_PATH)
+
+    pyblish.deregister_target("local")
