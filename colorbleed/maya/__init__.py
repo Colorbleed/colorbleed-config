50e5b29601cf17804a4f69b20a0a8d0fc6a82dad
Roy Nieterau <roy_nieterau@hotmail.com>
2019-07-16 16:29:23 +0200

    Hack in "Publish Remote..." entry to Avalon menu in Maya
    - Also reorganize code for headless mode slightly.

--- colorbleed/maya/__init__.py
+++ colorbleed/maya/__init__.py
@@ -4,8 +4,11 @@ import weakref
 
 from maya import utils, cmds, mel
 
+from avalon.maya.pipeline import (
+    IS_HEADLESS,
+    _menu as avalon_menu
+)
 from avalon import api as avalon, pipeline, maya
-from avalon.maya.pipeline import IS_HEADLESS
 from pyblish import api as pyblish
 
 from ..lib import (
@@ -31,17 +34,19 @@ def install():
     avalon.register_plugin_path(avalon.Loader, LOAD_PATH)
     avalon.register_plugin_path(avalon.Creator, CREATE_PATH)
 
-    menu.install()
-
+    # The `init` callback is required in headless mode to load referenced
+    # Alembics correctly at rendertime without errors.
     log.info("Installing callbacks ... ")
     avalon.on("init", on_init)
 
-    # Callbacks below are not required for headless mode, the `init` however
-    # is important to load referenced Alembics correctly at rendertime.
+    # The other Callbacks below are not required for headless mode
+    # Also menus and other UI cosmetics can be ignored in batch mode.
     if IS_HEADLESS:
         log.info("Running in headless mode, skipping Colorbleed Maya "
                  "save/open/new callback installation..")
         return
+
+    menu.install()
 
     avalon.on("save", on_save)
     avalon.on("open", on_open)
@@ -107,6 +112,11 @@ def on_init(_):
     cmds.loadPlugin("AbcImport", quiet=True)
     cmds.loadPlugin("AbcExport", quiet=True)
 
+    # Process everything below only when not in headless batch mode
+    # since they are purely cosmetic UI changes for the artists.
+    if IS_HEADLESS:
+        return
+
     # Force load objExport plug-in (requested by artists)
     cmds.loadPlugin("objExport", quiet=True)
 
@@ -115,9 +125,10 @@ def on_init(_):
         override_toolbox_ui
     )
     safe_deferred(override_component_mask_commands)
+    safe_deferred(override_toolbox_ui)
 
-    if not IS_HEADLESS:
-        safe_deferred(override_toolbox_ui)
+    # Add extra menu entry to Maya's Avalon Menu for "Remote Publish..."
+    safe_deferred(_add_remote_publish_to_avalon_menu)
 
 
 def on_before_save(return_code, _):
@@ -210,3 +221,39 @@ def on_task_changed(*args):
 
     # Run
     maya.pipeline._on_task_changed()
+
+
+def _add_remote_publish_to_avalon_menu():
+    """Add 'Remote Publish...' to Avalon menu"""
+
+    log.info("Customize Avalon menu: Refactor publish menu items..")
+
+    def _get_maya_menu_item_with_label(menu, label):
+        """Return menu item in menu that matches label"""
+        items = cmds.menu(avalon_menu, query=True, itemArray=True)
+        for item in items:
+            item_label = cmds.menuItem(item, query=True, label=True)
+            if item_label == label:
+                return item
+
+    # Get "Publish..." menu item
+    publish_menu_item = _get_maya_menu_item_with_label(
+        menu=avalon_menu,
+        label="Publish..."
+    )
+
+    # Copy icon from the Publish menu item..
+    icon = cmds.menuItem(publish_menu_item,
+                         query=True,
+                         image=True)
+
+    import pyblish_qml
+    cmds.menuItem("Remote publish...",
+                  command=lambda *args: pyblish_qml.show(
+                      targets=["default", "deadline"]
+                  ),
+                  image=icon,
+                  annotation="Submit scene to renderfarm to "
+                             "be published there..",
+                  insertAfter=publish_menu_item,
+                  parent=avalon_menu)
