f563d9bfa4af2c50b147f5a0f2109613ab522b7a
Roy Nieterau <roy_nieterau@hotmail.com>
2019-08-30 14:03:20 +0200

    Deprecate "visibleOnlyInFrameRange" data and instead use "visibleOnly"
    - The "visibleOnly" option was included a lot longer but never actually
      worked as it was a remainder from an older pipeline version. With this
      change things work again as they used to in the ancient pipeline.
    
    - It removes the redundant "visibleOnlyInFrameRange" which was
      implemented to do exactly what the "visibleOnly" originally should
      have done. This corrects that and deprecates "visibleOnlyInFrameRange"

--- colorbleed/plugins/maya/create/colorbleed_pointcache.py
+++ colorbleed/plugins/maya/create/colorbleed_pointcache.py
@@ -17,11 +17,10 @@ class CreatePointCache(avalon.maya.Creator):
 
         self.data["writeColorSets"] = False  # Vertex colors with the geometry.
         self.data["renderableOnly"] = False  # Only renderable visible shapes
-        self.data["visibleOnly"] = False     # only nodes that are visible
 
         # Include a more thorough check to ensure only nodes that are
         # are at least visible once throughout the frame range are included
-        self.data["visibleOnlyInFrameRange"] = False
+        self.data["visibleOnly"] = False
 
         self.data["includeParentHierarchy"] = False  # Include parent groups
         self.data["worldSpace"] = True       # Default to exporting world-space
