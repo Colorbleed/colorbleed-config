f06e74d975de1e8b86032c74d3fea2c2a3a442fb
Roy Nieterau <roy_nieterau@hotmail.com>
2019-11-18 16:03:51 +0100

    Keep original import namespace for code clarity

--- colorbleed/__init__.py
+++ colorbleed/__init__.py
@@ -1,7 +1,7 @@
 import os
 
-from pyblish import api as pyblish
-from avalon import api as avalon
+import pyblish.api
+import avalon.api
 
 from .launcher_actions import register_launcher_actions
 from .lib import collect_container_metadata
@@ -16,17 +16,17 @@ LOAD_PATH = os.path.join(PLUGINS_DIR, "global", "load")
 
 def install():
     print("Registering global plug-ins..")
-    pyblish.register_plugin_path(PUBLISH_PATH)
-    avalon.register_plugin_path(avalon.Loader, LOAD_PATH)
+    pyblish.api.register_plugin_path(PUBLISH_PATH)
+    avalon.api.register_plugin_path(avalon.api.Loader, LOAD_PATH)
 
     # Register default "local" target
     print("Registering pyblish target: local")
-    pyblish.register_target("local")
+    pyblish.api.register_target("local")
 
 
 def uninstall():
     print("Deregistering global plug-ins..")
-    pyblish.deregister_plugin_path(PUBLISH_PATH)
-    avalon.deregister_plugin_path(avalon.Loader, LOAD_PATH)
+    pyblish.api.deregister_plugin_path(PUBLISH_PATH)
+    avalon.api.deregister_plugin_path(avalon.api.Loader, LOAD_PATH)
 
-    pyblish.deregister_target("local")
+    pyblish.api.deregister_target("local")
