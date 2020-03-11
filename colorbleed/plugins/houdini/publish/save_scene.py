14a6d3d8773de4f49438934945680c468f34d464
Roy Nieterau <roy_nieterau@hotmail.com>
2020-03-11 11:12:34 +0100

    Implement save scene + incremental save for Houdini USD Render submissions

--- colorbleed/plugins/houdini/publish/save_scene.py
+++ colorbleed/plugins/houdini/publish/save_scene.py
@@ -1,23 +1,36 @@
 import pyblish.api
+import avalon.api
 
 
-class SaveCurrentScene(pyblish.api.ContextPlugin):
+class SaveCurrentScene(pyblish.api.InstancePlugin):
     """Save current scene"""
 
     label = "Save current file"
     order = pyblish.api.IntegratorOrder - 0.49
     hosts = ["houdini"]
-    targets = ["deadline"]
+    families = ["colorbleed.usdrender"]
+    targets = ["local"]
 
-    def process(self, context):
-        import hou
+    def process(self, instance):
 
-        assert context.data['currentFile'] == hou.hipFile.path(), (
+        # This should be a ContextPlugin, but this is a workaround
+        # for a bug in pyblish to run once for a family: issue #250
+        context = instance.context
+        key = "__hasRun{}".format(self.__class__.__name__)
+        if context.data.get(key, False):
+            return
+        else:
+            context.data[key] = True
+
+        # Filename must not have changed since collecting
+        host = avalon.api.registered_host()
+        current_file = host.current_file()
+        assert context.data['currentFile'] == current_file, (
             "Collected filename from current scene name."
         )
 
-        if hou.hipFile.hasUnsavedChanges():
+        if host.has_unsaved_changes():
             self.log.info("Saving current file..")
-            hou.hipFile.save(save_to_recent_files=True)
+            host.save_file(current_file)
         else:
             self.log.debug("No unsaved changes, skipping file save..")
