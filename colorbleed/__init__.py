0b22248543c89a94cae200d5f4c506f9f979fc75
Roy Nieterau <roy_nieterau@hotmail.com>
2020-04-08 16:50:55 +0200

    Implement quick workaround for project-specific environment overrides

--- colorbleed/__init__.py
+++ colorbleed/__init__.py
@@ -1,4 +1,5 @@
 import os
+import json
 
 import pyblish.api
 import avalon.api
@@ -26,6 +27,21 @@ def install():
     # Set a Pyblish QML post collector order: pyblish-qml#356
     post_collector_order = str(pyblish.api.CollectorOrder + 0.3)
     os.environ["PYBLISH_QML_POST_COLLECT"] = post_collector_order
+
+    # Try and set project specific environment if it has implemented any
+    # todo: implement better project-specific settings
+    template = "{AVALON_PROJECTS}/{AVALON_PROJECT}/resources/pipeline/env.json"
+    override_path = template.format(**avalon.api.Session)
+    if os.path.isfile(override_path):
+        print("Loading project env override: %s" % override_path)
+        try:
+            with open(override_path, "r") as f:
+                override = json.load(f)
+                override = {str(key): str(value) for key, value in
+                            override.items()}
+                os.environ.update(override)
+        except Exception as exc:
+            print(exc)
 
 
 def uninstall():
