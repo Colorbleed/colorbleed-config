2916447bcabf7e6089028f061d2f7df6ed168c6c
Roy Nieterau <roy_nieterau@hotmail.com>
2020-04-10 15:43:35 +0200

    Write USD with colorbleed.animation
    - Fix start or end frame 0 disallowing to export the frame range
    - Add in more options to disable/enable for the instance
    - Suspend refresh during extraction
    - Add in temporary hack to avoid UVs in animation publish

--- colorbleed/plugins/maya/publish/extract_usd.py
+++ colorbleed/plugins/maya/publish/extract_usd.py
@@ -11,15 +11,24 @@ from colorbleed.maya.lib import get_highest_in_hierarchy
 class ExtractUSD(colorbleed.api.Extractor):
     """Extract USD
 
-    This uses the Multiverse 6.0 plug-in.
+    This uses the Multiverse 6.3.0+ plug-in.
 
     """
 
     label = "Extract USD"
     hosts = ["maya"]
-    families = ["usdModel", "usdPointcache"]
+    families = ["usdModel", "usdPointcache", "colorbleed.animation"]
 
     def process(self, instance):
+
+        if ("outMembers" in instance.data and
+                "outMembersHierarchy" in instance.data):
+            # The animation family collects members using the out_SET
+            roots = instance.data["outMembers"]
+            nodes = instance.data["outMembersHierarchy"]
+        else:
+            roots = instance.data["setMembers"]
+            nodes = instance[:]
 
         # Ensure the plugin is loaded
         cmds.loadPlugin("MultiverseForMaya", quiet=True)
@@ -29,11 +38,9 @@ class ExtractUSD(colorbleed.api.Extractor):
         filename = "{0}.usd".format(instance.name)
         path = os.path.join(dir_path, filename)
 
-        write_ancestors = instance.data.get("includeParentHierarchy", False)
-
         # Use time samples when provided
         kwargs = {}
-        if instance.data.get("startFrame") and instance.data.get("endFrame"):
+        if "startFrame" in instance.data and "endFrame" in instance.data:
             start = instance.data["startFrame"]
             end = instance.data["endFrame"]
             handles = instance.data.get("handles", 0)
@@ -45,24 +52,42 @@ class ExtractUSD(colorbleed.api.Extractor):
             kwargs["numTimeSamples"] = 1
             kwargs["timeSamplesSpan"] = 0.0
 
-            # Perform extraction
+        # Add some kwargs that are customizable options as instance data
+        options = {
+            "writeMeshes": True,
+            "writePositions": True,
+            "writeUVs": True,
+            "writeNormals": True,
+            "writeTransformMatrix": True,
+            "writeCameras": False,
+            "writeColorSets": False,
+            "writeDisplayColor": False,
+            "stripNamespaces": True
+        }
+        for key in options:
+            if key in instance.data:
+                options[key] = instance.data[key]
+        kwargs.update(options)
+
+        # todo: move this code elsewhere
+        if ("colorbleed.animation" == instance.data.get("family") or
+            "colorbleed.animation" in instance.data.get("families", [])):
+            self.log.debug("Disabling writing UVs..")
+            kwargs["writeUVs"] = False
+
+        roots = get_highest_in_hierarchy(roots)
+        write_ancestors = instance.data.get("includeParentHierarchy", False)
+
+        # Perform extraction
         self.log.info("Performing extraction..")
-        with avalon.maya.maintained_selection():
-            cmds.select(instance, noExpand=True)
-
-            root = get_highest_in_hierarchy(instance)
-
-            cmds.mvUsdWriteAsset(assetPath=path,
-                                 dagRoot=root,
-                                 writeAncestors=write_ancestors,
-                                 writeTransformMatrix=True,
-                                 writePositions=True,
-                                 writeMeshes=True,
-                                 writeNormals=True,
-                                 writeUVs=True,
-                                 writeDisplayColor=False,
-                                 mergeTransformAndShape=True,
-                                 **kwargs)
+        with avalon.maya.suspended_refresh():
+            with avalon.maya.maintained_selection():
+                cmds.select(nodes, noExpand=True)
+                cmds.mvUsdWriteAsset(assetPath=path,
+                                     dagRoot=roots,
+                                     writeAncestors=write_ancestors,
+                                     mergeTransformAndShape=True,
+                                     **kwargs)
 
         if "files" not in instance.data:
             instance.data["files"] = list()
