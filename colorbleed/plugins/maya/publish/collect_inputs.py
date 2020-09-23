37a790dcb8752b1b8dfd51447b7a47c8da007684
Roy Nieterau <roy_nieterau@hotmail.com>
2020-09-23 18:01:39 +0200

    Include history for input collection, move order so it runs as second pass

--- colorbleed/plugins/maya/publish/collect_inputs.py
+++ colorbleed/plugins/maya/publish/collect_inputs.py
@@ -1,11 +1,84 @@
 import copy
 from maya import cmds
+import maya.api.OpenMaya as om
 
 from colorbleed.maya import lib
 
 import avalon.io as io
 import avalon.api as api
 import pyblish.api
+
+
+def get_history(nodes,
+                filter=om.MFn.kInvalid,
+                direction=om.MItDependencyGraph.kUpstream):
+    """Get upstream history for list of nodes.
+
+    This acts as a replacement to maya.cmds.listHistory.
+    It's faster by about 2x-3x. It returns less than
+    maya.cmds.listHistory as it excludes the input nodes
+    from the output (unless an input node was history
+    for another input node). It also excludes duplicates.
+
+    Args:
+        nodes (list): Maya node names to start search from.
+        filter (om.MFn.Type): Filter to only specific types.
+            e.g. to dag nodes using om.MFn.kDagNode
+        direction (om.MItDependencyGraph.Direction): Direction to traverse in.
+            Defaults to upstream.
+
+    Returns:
+        list: Node names in upstream history.
+
+    """
+    if not nodes:
+        return []
+
+    sel = om.MSelectionList()
+    for node in nodes:
+        sel.add(node)
+
+    it = om.MItDependencyGraph(sel.getDependNode(0))  # init iterator
+    handle = om.MObjectHandle
+
+    traversed = set()
+    fn_dep = om.MFnDependencyNode()
+    fn_dag = om.MFnDagNode()
+    result = []
+    for i in range(sel.length()):
+
+        start_node = sel.getDependNode(i)
+        start_node_hash = handle(start_node).hashCode()
+        if start_node_hash in traversed:
+            continue
+
+        it.resetTo(start_node,
+                   filter=filter,
+                   direction=direction)
+        while not it.isDone():
+
+            node = it.currentNode()
+            node_hash = handle(node).hashCode()
+
+            if node_hash in traversed:
+                it.prune()
+                it.next()
+                continue
+
+            traversed.add(node_hash)
+
+            if node.hasFn(om.MFn.kDagNode):
+                fn_dag.setObject(node)
+                name = fn_dag.fullPathName()
+                result.append(name)
+            else:
+                fn_dep.setObject(node)
+                name = fn_dep.name()
+                result.append(name)
+
+            it.next()
+
+    return result
 
 
 def collect_input_containers(containers, nodes):
@@ -51,7 +124,7 @@ class CollectUpstreamInputs(pyblish.api.InstancePlugin):
     """
 
     label = "Collect Inputs"
-    order = pyblish.api.CollectorOrder + 0.2
+    order = pyblish.api.CollectorOrder + 0.34
     hosts = ["maya"]
 
     def process(self, instance):
@@ -80,9 +153,30 @@ class CollectUpstreamInputs(pyblish.api.InstancePlugin):
             # Basic behavior
             nodes = instance[:]
 
-            # Include any input connections of history
-            history = cmds.ls(cmds.listHistory(nodes), long=True)
-            nodes = list(set(nodes + history))
+            # Include any input connections of history with long names
+            # For optimization purposes only trace upstream from shape nodes
+            # looking for used dag nodes. This way having just a constraint
+            # on a transform is also ignored which tended to give irrelevant
+            # inputs for the majority of our use cases. We tend to care more
+            # about geometry inputs.
+            shapes = cmds.ls(nodes,
+                             type=("mesh", "nurbsSurface", "nurbsCurve"),
+                             noIntermediate=True)
+            if shapes:
+                history = get_history(shapes, filter=om.MFn.kShape)
+                history = cmds.ls(history, long=True)
+
+                # Include the transforms in the collected history as shapes
+                # are excluded from containers
+                transforms = cmds.listRelatives(cmds.ls(history, shapes=True),
+                                                parent=True,
+                                                fullPath=True,
+                                                type="transform")
+                if transforms:
+                    history.extend(transforms)
+
+                if history:
+                    nodes = list(set(nodes + history))
 
             # Collect containers for the given set of nodes
             containers = collect_input_containers(scene_containers,
@@ -135,5 +229,4 @@ class CollectUpstreamInputs(pyblish.api.InstancePlugin):
             containers = collect_input_containers(scene_containers, members)
 
         return containers
-
 
