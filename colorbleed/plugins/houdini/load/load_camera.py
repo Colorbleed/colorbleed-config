b02d3f2663be386ce124dbac6d22e09cb7e8dbdb
Roy Nieterau <roy_nieterau@hotmail.com>
2021-12-14 17:56:17 +0100

    Cosmetics

--- colorbleed/plugins/houdini/load/load_camera.py
+++ colorbleed/plugins/houdini/load/load_camera.py
@@ -5,59 +5,62 @@ from avalon.houdini import pipeline, lib
 
 import hou
 
-ARCHIVE_EXPRESSION = '__import__("_alembic_hom_extensions").alembicGetCameraDict'
+ARCHIVE_EXPRESSION = (
+    '__import__("_alembic_hom_extensions").alembicGetCameraDict'
+)
+
 
 def transfer_non_default_values(src, dest, ignore=None):
     """Copy parm from src to dest.
-    
+
     Because the Alembic Archive rebuilds the entire node
     hierarchy on triggering "Build Hierarchy" we want to
     preserve any local tweaks made by the user on the camera
     for ease of use. That could be a background image, a
     resolution change or even Redshift camera parameters.
-    
+
     We try to do so by finding all Parms that exist on both
     source and destination node, include only those that both
     are not at their default value, they must be visible,
     we exclude those that have the special "alembic archive"
     channel expression and ignore certain Parm types.
-    
+
     """
 
-    src.updateParmStates()  
+    src.updateParmStates()
 
     for parm in src.allParms():
-    
+
         if ignore and parm.name() in ignore:
             continue
-    
+
         # If destination parm does not exist, ignore..
         dest_parm = dest.parm(parm.name())
         if not dest_parm:
             continue
-    
+
         # Ignore values that are currently at default
         if parm.isAtDefault() and dest_parm.isAtDefault():
             continue
-            
+
         if not parm.isVisible():
             # Ignore hidden parameters, assume they
             # are implementation details
             continue
-            
+
         expression = None
         try:
             expression = parm.expression()
         except hou.OperationFailed:
             # No expression present
             pass
-            
+
         if expression is not None and ARCHIVE_EXPRESSION in expression:
-            # Assume it's part of the automated connections that the Alembic Archive
-            # makes on loading of the camera and thus we do not want to transfer
-            # the expression
+            # Assume it's part of the automated connections that the Alembic
+            # Archive makes on loading of the camera and thus we do not want
+            # to transfer the expression
             continue
-            
+
         # Ignore folders, separators, etc.
         ignore_types = {
             hou.parmTemplateType.Toggle,
@@ -69,7 +72,7 @@ def transfer_non_default_values(src, dest, ignore=None):
         }
         if parm.parmTemplate().type() in ignore_types:
             continue
-            
+
         print("Preserving attribute: %s" % parm.name())
         dest_parm.setFromParm(parm)
 
@@ -135,7 +138,7 @@ class CameraLoader(api.Loader):
                        "representation": str(representation["_id"])})
 
         # Store the cam temporarily next to the Alembic Archive
-        # so that we can preserve parm values the user set on it 
+        # so that we can preserve parm values the user set on it
         # after build hierarchy was triggered.
         old_camera = self._get_camera(node)
         temp_camera = old_camera.copyTo(node.parent())
@@ -145,13 +148,13 @@ class CameraLoader(api.Loader):
 
         # Apply values to the new camera
         new_camera = self._get_camera(node)
-        transfer_non_default_values(temp_camera, 
+        transfer_non_default_values(temp_camera,
                                     new_camera,
-                                    # The hidden uniform scale attribute 
+                                    # The hidden uniform scale attribute
                                     # gets a default connection to "icon_scale"
                                     # just skip that completely
                                     ignore={"scale"})
-            
+
         temp_camera.destroy()
 
     def remove(self, container):
