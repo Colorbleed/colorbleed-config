2d1f60082c4360c6b4d011bbe49cf3c9a0f9855a
Roy Nieterau <roy_nieterau@hotmail.com>
2021-12-16 15:34:30 +0100

    Preserve camera in Maya modelPanels on camera update

--- colorbleed/plugins/maya/load/load_camera.py
+++ colorbleed/plugins/maya/load/load_camera.py
@@ -1,4 +1,5 @@
 import colorbleed.maya.plugin
+import colorbleed.maya.lib as lib
 
 
 class CameraLoader(colorbleed.maya.plugin.ReferenceLoader):
@@ -40,3 +41,36 @@ class CameraLoader(colorbleed.maya.plugin.ReferenceLoader):
         self[:] = nodes
 
         return nodes
+
+    def update(self, container, representation):
+
+        from maya import cmds
+
+        # Get the modelPanels that used the old camera
+        members = lib.get_container_members(container)
+        old_cameras = cmds.ls(members, type="camera", long=True)
+        update_panels = []
+        for panel in cmds.getPanel(type="modelPanel"):
+            cam = cmds.ls(cmds.modelPanel(panel, query=True, camera=True),
+                          long=True)
+
+            # Often but not always maya returns the transform from the
+            # modelPanel as opposed to the camera shape, so we convert it
+            # to explicitly be the camera shape
+            if cmds.nodeType(cam) != "camera":
+                cam = cmds.listRelatives(cam,
+                                         children=True,
+                                         fullPath=True,
+                                         type="camera")[0]
+            if cam in old_cameras:
+                update_panels.append(panel)
+
+        # Perform regular reference update
+        super(CameraLoader, self).update(container, representation)
+
+        # Update the modelPanels to contain the new camera
+        members = lib.get_container_members(container)
+        new_cameras = cmds.ls(members, type="camera", long=True)
+        new_camera = new_cameras[0]
+        for panel in update_panels:
+            cmds.modelPanel(panel, edit=True, camera=new_camera)
